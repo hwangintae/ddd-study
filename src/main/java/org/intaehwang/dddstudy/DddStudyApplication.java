ee7b58f796e3a17a57a685fcadcde193da22ba06
intaeHwang <hwang9029@ckinform.com>
2025-04-14 12:55:42 +0900

    Initial commit

---
+++ src/main/java/org/intaehwang/dddstudy/DddStudyApplication.java
@@ -0,0 +1,13 @@
+package org.intaehwang.dddstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DddStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DddStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/intaehwang/dddstudy/DddStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package org.intaehwang.dddstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DddStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
