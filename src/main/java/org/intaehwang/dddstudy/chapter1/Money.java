9337146b02ac14eb2146460fcc8a5f9127feb4b6
intaeHwang <hwang9029@ckinform.com>
2025-04-15 13:00:08 +0900

    feat/ Order, OrderLine 기능 및 필드 추가

---
+++ src/main/java/org/intaehwang/dddstudy/chapter1/Money.java
@@ -0,0 +1,9 @@
+package org.intaehwang.dddstudy.chapter1;
+
+public class Money {
+    private int money;
+
+    public Money(int money) {
+        this.money = money;
+    }
+}
--- src/main/java/org/intaehwang/dddstudy/chapter1/Order.java
+++ src/main/java/org/intaehwang/dddstudy/chapter1/Order.java
@@ -1,8 +1,17 @@
 package org.intaehwang.dddstudy.chapter1;
 
+import java.util.List;
+
 public class Order {
+    private List<OrderLine> orderLines;
+    private Money totalAmounts;
+
     private OrderState state;
     private ShippingInfo shippingInfo;
+
+    public Order(List<OrderLine> orderLines) {
+        setOrderLines(orderLines);
+    }
 
     public Order(OrderState state, ShippingInfo shippingInfo) {
         this.state = state;
@@ -20,5 +29,28 @@ public class Order {
     private boolean isShippingChangeable() {
         return state == OrderState.PAYMENT_WAITING ||
                 state == OrderState.PREPARING;
+    }
+
+    public void changeShipped() {}
+    public void cancel() {}
+    public void completePayment() {}
+
+    private void setOrderLines(List<OrderLine> orderLines) {
+        verifyAtLestOneOrMoreOrderLines(orderLines);
+        this.orderLines = orderLines;
+        calculateTotalAmounts();
+    }
+
+    private void verifyAtLestOneOrMoreOrderLines(List<OrderLine> orderLines) {
+        if (orderLines == null || orderLines.isEmpty()) {
+            throw new IllegalArgumentException("no OrderLine");
+        }
+    }
+
+    private void calculateTotalAmounts() {
+        int sum = orderLines.stream()
+                .mapToInt(OrderLine::getAmounts)
+                .sum();
+        this.totalAmounts = new Money(sum);
     }
 }
