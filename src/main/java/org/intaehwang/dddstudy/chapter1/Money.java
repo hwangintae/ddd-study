4843bd1192d57818580fea670fed9c0af63b4507
intaeHwang <hwang9029@ckinform.com>
2025-04-17 12:42:26 +0900

    feat/ Money에 add, multiply 구현

--- src/main/java/org/intaehwang/dddstudy/chapter1/Money.java
+++ src/main/java/org/intaehwang/dddstudy/chapter1/Money.java
@@ -1,9 +1,20 @@
 package org.intaehwang.dddstudy.chapter1;
 
-public class Money {
-    private int money;
+import lombok.Getter;
 
-    public Money(int money) {
-        this.money = money;
+@Getter
+public class Money {
+    private int value;
+
+    public Money(int value) {
+        this.value = value;
+    }
+
+    public Money add(Money money) {
+        return new Money(this.value + money.value);
+    }
+
+    public Money multiply(int multiplier) {
+        return new Money(this.value * multiplier);
     }
 }
--- src/main/java/org/intaehwang/dddstudy/chapter1/OrderLine.java
+++ src/main/java/org/intaehwang/dddstudy/chapter1/OrderLine.java
@@ -2,22 +2,22 @@ package org.intaehwang.dddstudy.chapter1;
 
 public class OrderLine {
     private Product product;
-    private int price;
+    private Money price;
     private int quantity;
-    private int amounts;
+    private Money amounts;
 
-    public OrderLine(Product product, int price, int quantity) {
+    public OrderLine(Product product, Money price, int quantity) {
         this.product = product;
-        this.price = price;
+        this.price = new Money(price.getValue());
         this.quantity = quantity;
         this.amounts = calculateAmounts();
     }
 
-    private int calculateAmounts() {
-        return price * quantity;
+    private Money calculateAmounts() {
+        return price.multiply(quantity);
     }
 
     public int getAmounts() {
-        return amounts;
+        return amounts.getValue();
     }
 }
