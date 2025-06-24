a88e93a9e6c255dda9769d4898e965ce3e7bed14
intaeHwang <hwang9029@ckinform.com>
2025-06-24 21:45:01 +0900

    feat/ 포워드 구현

---
+++ src/main/java/org/intaehwang/dddstudy/chapter10/EventSender.java
@@ -0,0 +1,10 @@
+package org.intaehwang.dddstudy.chapter10;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class EventSender {
+    public void send(EventEntry entry) {
+
+    }
+}
